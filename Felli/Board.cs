dae8b5048b36b2c394781d57d6088e0784eac47f
IDunnoRagna <afonsomnmteixeira@gmail.com>
2020-05-12 16:18:05 +0100

    Added board and program

---
+++ Felli/Board.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Felli
+{
+    // Board class, stores the positions of all pieces on the map.
+    class Board
+    {
+        public const int Empty = UnravelledBoard.Empty;
+        public const int White = UnravelledBoard.White;
+        public const int Black = UnravelledBoard.Black;
+        public const int Wall = UnravelledBoard.Wall;
+
+        // The unravelled board.
+        UnravelledBoard board;
+
+        // Board constructor.
+        public Board()
+        {
+            // Create a new standard board.
+            board = new UnravelledBoard();
+        }
+    }
+}
---
+++ Felli/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Felli
+{
+    // Main class for the whole program
+    class Program
+    {
+        // Entry function
+        static void Main(string[] args)
+        {
+            // Set console to accept 'utf-8'
+            // so we can put weird character in like the full bar and stuff
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            // Create a new board for the whole game
+            Board board = new Board();
+
+            // Wait for newkey
+            // Console.WriteLine("Press enter to exit!");
+            // Console.ReadLine();
+        }
+    }
+}
